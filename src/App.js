f9dc7a9567a1cf1cedf6b5da9781da271d09cd9a
Manuel Valles <manukempo@gmail.com>
2018-01-28 19:50:26 +0000

    Added Axios and Bacon Ipsum

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
+// Import Axios - Promise based HTTP client for the browser and node.js
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      paras: 5,
+      format: 'html',
+      text: ''
+    }
+
+  }
+  // componentWhileMount as we make a request to an API 
+  componentWillMount(){
+    this.getSampleText();
+  }
+  
+  getSampleText(){
+    axios.get('https://baconipsum.com/api/?type=meat-and-filler&paras='+this.state.paras+'&format=html')
+    .then(response=>{
+      this.setState({text: response.data}, function(){
+        console.log(this.state);
+      });
+    })
+    .catch(err=>{
+      console.log(err);
+    })
+  }
   render() {
     return (
       <div className="App">
