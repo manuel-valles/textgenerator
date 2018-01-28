158649cfeaef5f39149f0af03b52a4cac198fd45
Manuel Valles <manukempo@gmail.com>
2018-01-28 20:40:50 +0000

    Select Controller - HTML/Text

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import axios from 'axios';
 // Import the Output Component
 import Output from './Components/Output';
+// Import the Select Component
+import Select from './Components/Controls/Select';
 
 class App extends Component {
   constructor(props){
@@ -22,7 +24,7 @@ class App extends Component {
   }
   
   getSampleText(){
-    axios.get('https://baconipsum.com/api/?type=meat-and-filler&paras='+this.state.paras+'&format=html')
+    axios.get('https://baconipsum.com/api/?type=meat-and-filler&paras='+this.state.paras+'&format='+this.state.format)
     .then(response=>{
       this.setState({text: response.data}, function(){
         console.log(this.state);
@@ -32,9 +34,23 @@ class App extends Component {
       console.log(err);
     })
   }
+
+  showHtml(x){
+    // change value and return a new sample text
+    this.setState({format: x}, this.getSampleText);
+  }
   render() {
     return (
-      <div className="App">
+      // Add container class to center everything
+      <div className="App container">
+        <h1>Text Generator & React</h1>
+        <hr/>
+        <form className="form-inline">
+          <div className="form-group">
+            <label>Output Format</label>
+            <Select value={this.state.format} onChange={this.showHtml.bind(this)}/>
+          </div>
+        </form>
         <Output value={this.state.text}/>
       </div>
     );
---
+++ src/Components/Controls/Select.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+
+class Select extends Component{
+	constructor(props){
+		super(props);
+
+		this.state = {
+			value: props.value
+		}
+	}
+
+	onChange(e){
+		this.setState({value: e.target.value}, function(){
+			this.props.onChange(this.state.value);
+		})
+	}
+
+	render(){
+		return(
+			<div>
+				<select className="form-control" onChange={this.onChange.bind(this)}>
+					<option value="html">Html</option>
+					<option value="text">Text</option>
+				</select>
+			</div>
+		);
+	}
+}
+
+export default Select;
